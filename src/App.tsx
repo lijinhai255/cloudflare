d725e3986b5af7c5e2bb07fbf5b4a6f9aefb5457
lijinhai255 <18854109500@163.com>
2024-10-20 18:45:37 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+
+function App() {
+  const [title, getTitle] = useState<"">("");
+  useEffect(() => {
+    fetch("https://yd-cloudflare-server-ts.9760414764447.workers.dev/")
+      .then((response: Response) => response?.json())
+      .then((data) => {
+        // let obj = JSON.parse(data);
+        console.log(data);
+        getTitle(data);
+      });
+  }, []);
+  return (
+    <div className="App">
+      <h2>标题</h2>
+      {title}
+    </div>
+  );
+}
+
+export default App;
